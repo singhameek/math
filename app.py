a8a668686d4875885f1c0a5280793847f8b353fb
singhameek <ameeksinghv@proton.me>
2026-08-19 19:48:00 +0000

    finished sim, trig

--- app.py
+++ app.py
@@ -39,3 +39,75 @@ elif mode == "Simultaneous":
         d = st.number_input("d", value=0.0, key="d2")
         e = st.number_input("e", value=0.0, key="e2")
         f = st.number_input("f", value=0.0, key ="f2")
+
+    if st.button("Solve", key="btn_sim"):
+        D = (a*e)-(b*d)
+
+        if D != 0:
+            x_res = ((c*e)-(b*f))/D
+            y_res = ((a*f)-(c*d))/D
+            st.success(f"Results: $x = {round(x_res, 4)}$, $y = {round(y_res, 4)}$")
+
+            fig, ax = plt.subplots(figsizw=(8,8))
+            limit = max(abs(x_res), abs(y_res), 10) + 2
+            x_vals = np.linspace(-limit, limit, 400)
+
+            ax.spines['left'].set_position('zero')
+            ax.spines['bottom'].set_position('zero')
+            ax.spines['right'].set_color('none')
+            ax.spines['top'].set_color('none')
+
+
+            if b != 0:
+                y1 = (c-a*x_vals)/b
+                ax.plot(x_vals, y1, label=f'Line 1: {a}x + {b}y = {c}')
+            else:
+                ax.axvline(x=c/a, color='blue', label=f'line 1: {a}x = {c}')
+
+            ax.plot(x_res, y_res, 'ro', markersize=10, label='Intersection', zorder=5)
+
+            ax.set_xlim(-limit, limit)
+            ax.set_ylim(-limit, limit)
+            ax.set_aspect('equal')
+            ax.grid(True, linestyle='--', alpha = 0.5)
+            ax.legend(loc='upper right')
+
+            st.pyplot(fig)
+
+elif mode == "Trig":
+    st.header("Solve Trig Functions")
+    trigfunc = st.selectbox("Choose function", ["sine", "cosine", "tangent", "inverse sine", "inverse cosine", "inverse tangent"])
+    value = st.number_input("Value (Degrees for sin/cos/tan, ratio for inverse)", value = 0.0, key = "value")
+
+    if st.button("Solve", key = "btn_trig"):
+        if trigfunc == "sine":
+            result = math.sin(math.radians(value))
+            st.success(f"Result: {round(result, 4)}")
+
+        elif trigfunc == "cosine":
+            result = math.cos(math.radians(value))
+            st.success(f"Result: {round(result, 4)}")
+
+        elif trigfunc == "tangent":
+            result = math.tan(math.radians(value))
+            st.success(f"Result: {round(result, 4)}")
+
+        elif trigfunc == "inverse sine":
+            if -1<= value <=1:
+                result_deg = math.degrees(math.acos(value))
+                st.success(f"Result: {round(result_deg, 4)}")
+            else:
+                st.error("Value must be between 1 and -1")
+
+        elif trigfunc == "inverse cosine":
+            if -1<= value <=1:
+                result_deg = math.degrees(math.acos(value))
+                st.success(f"Result: {round(result_deg, 4)}")
+            else:
+                st.error("Value must be between 1 and -1")
+
+        elif trigfunc == "inverse tangent":
+            result_deg = math.degrees(math.atan(value))
+            st.success(f"Result: {round(result_deg, 4)}")
+
+        
