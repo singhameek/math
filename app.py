d93f3c8df3be5a28836d1207cde80b3df7f43e05
singhameek <ameeksinghv@proton.me>
2026-08-19 19:21:25 +0000

    linear equations, sim wip

--- app.py
+++ app.py
@@ -10,3 +10,32 @@ from scipy import stats
 st.set_page_config(layout="wide")
 st.set_page_config(page_title="Math", page_icon="+")
 st.title("Math Solver")
+
+mode = st.sidebar.selection("Select Mode", ["Single Equation", "Simultaneous", "Trig", "Quadratics", "Calculus", "Stats", "Unit Conversion"])
+
+if mode == "Single Equation":
+    st.header("Solve $ax +b = c$")
+    a = st.number_input("Value for a", value=0.0, key="a1")
+    b = st.number_input("Value for b", value=0.0, key="b1")
+    c = st.number_input("Value for c", value=0.0, key="b1")
+
+    if st.button("Solve", key="btn_single"):
+        if a == 0:
+            st.error("a cannot be 0")
+        else:
+            x=(c-b)/a
+            st.success(f"Result: $x = {round(x,4)}")
+
+elif mode == "Simultaneous":
+    st.header("Solve $ax+by=c$ and $dx+ey=f$")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        a = st.number_input("a", value=0.0, key="a2")
+        b = st.number_input("b", value = 0.0, key="b2")
+        c = st.number_input("c", value =0.0, key="c2")
+
+    with col2:
+        d = st.number_input("d", value=0.0, key="d2")
+        e = st.number_input("e", value=0.0, key="e2")
+        f = st.number_input("f", value=0.0, key ="f2")
