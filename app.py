c4713dd511b085f293d22efffcf8655950a3b369
singhameek <ameeksinghv@proton.me>
2026-08-19 19:11:16 +0000

    req.txt

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import sympy as sp
+import pandas as pd
+from scipy import stats
+
+
+st.set_page_config(layout="wide")
+st.set_page_config(page_title="Math", page_icon="+")
+st.title("Math Solver")
