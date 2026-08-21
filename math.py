2f08d2c6541e55a1c6aa2df2eb96db1b38c523af
singhameek <ameeksinghv@proton.me>
2026-08-21 00:48:43 +0000

    linear complete

--- math.py
+++ math.py
@@ -1,3 +1,16 @@
 import math
 
-# LINEAR #
+mode = int(input("Please choose mode: 1 for linear, 2 for simultaneous, 3 for quadratics, 4 for trigonometry, 5 for statistics \n"))
+
+
+if mode == 1:
+    print("Solving for ax + b = c")
+    a = int(input("Value for a \n"))
+    b = int(input("Value for b \n"))
+    c = int(input("Value for c \n"))
+
+    if a == 0:
+        print("The coefficient = 0,",b,"=",c)
+    else:
+        answer = (c - b) / a
+        print("x is equal to", answer)
